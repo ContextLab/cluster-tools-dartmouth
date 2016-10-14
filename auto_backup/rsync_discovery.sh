eab382dae4da14302ae0fb4c3b7d5052afb42e5a
Kirsten Ziman <kirstenkmbziman@gmail.com>
2016-10-14 15:56:30 -0400

    Update rsync_discovery.sh

--- auto_backup/rsync_discovery.sh
+++ auto_backup/rsync_discovery.sh
@@ -1,5 +1,5 @@
-#this script will transfer the desired file/directory to discovery, provided you have granted access to your discovery account
-#it will also auto-email a log of the transfer
+#this script will copy the desired file/directory to discovery, provided you have granted access to your discovery account
+#it will also auto-email a log of the backup
 
 rsync -av audio kziman@discovery.dartmouth.edu:/idata/cdl/data/behavioral/FRFR/synced_files_from_labmac/ > logfile
 rsync -av participants.db kziman@discovery.dartmouth.edu:/idata/cdl/data/behavioral/FRFR/synced_files_from_labmac/ >> logfile
