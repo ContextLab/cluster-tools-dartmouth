76264224ed88a5cca5beca05cd5360812a6d113b
Jeremy R. Manning <jeremy.r.manning@dartmouth.edu>
2016-11-30 17:58:10 -0500

    voxel-wise analysis (activity based decoding)

---
+++ pieman_cluster_bootstrap_stats_voxel.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python
+
+# create a bunch of job scripts
+import datetime as dt
+import getpass
+import os
+import socket
+from subprocess import call
+
+# ====== MODIFY ONLY THE CODE BETWEEN THESE LINES ======
+import numpy as np
+import scipy.io as sio
+from config import config
+
+data = sio.loadmat(os.path.join(config['datadir'], 'pieman_data.mat'))
+ignore_keys = ('__header__', '__globals__', '__version__')
+conditions = set(data.keys()) - set(ignore_keys)
+
+n_iterations = 100
+
+job_script = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pieman_barplot_cruncher_voxel.py')
+job_commands = list()
+job_names = list()
+for c in conditions:
+    for i in np.arange(n_iterations):
+        job_commands.append(job_script + " " + str(i) + " " + c)
+        job_names.append('pieman_barplot_voxel_' + c + "_iter" + str(i) + '.sh')
+# ====== MODIFY ONLY THE CODE BETWEEN THESE LINES ======
+
+assert(len(job_commands) == len(job_names))
+
+
+# job_command is referenced in the run_job.sh script
+# noinspection PyBroadException,PyUnusedLocal
+def create_job(name, job_command):
+    # noinspection PyUnusedLocal,PyShadowingNames
+    def create_helper(s, job_command):
+        x = [i for i, char in enumerate(s) if char == '<']
+        y = [i for i, char in enumerate(s) if char == '>']
+        if len(x) == 0:
+            return s
+
+        q = ''
+        index = 0
+        for i in range(len(x)):
+            q += s[index:x[i]]
+            unpacked = eval(s[x[i]+1:y[i]])
+            q += str(unpacked)
+            index = y[i]+1
+        return q
+
+    # create script directory if it doesn't already exist
+    try:
+        os.stat(config['scriptdir'])
+    except:
+        os.makedirs(config['scriptdir'])
+
+    template_fd = open(config['template'], 'r')
+    job_fname = os.path.join(config['scriptdir'], name)
+    new_fd = open(job_fname, 'w+')
+
+    while True:
+        next_line = template_fd.readline()
+        if len(next_line) == 0:
+            break
+        new_fd.writelines(create_helper(next_line, job_command))
+    template_fd.close()
+    new_fd.close()
+    return job_fname
+
+
+# noinspection PyBroadException
+def lock(lockfile):
+    try:
+        os.stat(lockfile)
+        return False
+    except:
+        fd = open(lockfile, 'w')
+        fd.writelines('LOCK CREATE TIME: ' + str(dt.datetime.now()) + '\n')
+        fd.writelines('HOST: ' + socket.gethostname() + '\n')
+        fd.writelines('USER: ' + getpass.getuser() + '\n')
+        fd.writelines('\n-----\nCONFIG\n-----\n')
+        for k in config.keys():
+            fd.writelines(k.upper() + ': ' + str(config[k]) + '\n')
+        fd.close()
+        return True
+
+
+# noinspection PyBroadException
+def release(lockfile):
+    try:
+        os.stat(lockfile)
+        os.remove(lockfile)
+        return True
+    except:
+        return False
+
+
+script_dir = config['scriptdir']
+lock_dir = config['lockdir']
+lock_dir_exists = False
+# noinspection PyBroadException
+try:
+    os.stat(lock_dir)
+    lock_dir_exists = True
+except:
+    os.makedirs(lock_dir)
+
+# noinspection PyBroadException
+try:
+    os.stat(config['startdir'])
+except:
+    os.makedirs(config['startdir'])
+
+locks = list()
+for n, c in zip(job_names, job_commands):
+    # if the submission script crashes before all jobs are submitted, the lockfile system ensures that only
+    # not-yet-submitted jobs will be submitted the next time this script runs
+    next_lockfile = os.path.join(lock_dir, n+'.LOCK')
+    locks.append(next_lockfile)
+    if not os.path.isfile(os.path.join(script_dir, n)):
+        if lock(next_lockfile):
+            next_job = create_job(n, c)
+
+            if (socket.gethostname() == 'discovery') or (socket.gethostname() == 'ndoli'):
+                submit_command = 'echo "[SUBMITTING JOB: ' + next_job + ']"; qsub'
+            else:
+                submit_command = 'echo "[RUNNING JOB: ' + next_job + ']"; sh'
+
+            call(submit_command + " " + next_job, shell=True)
+
+# all jobs have been submitted; release all locks
+for l in locks:
+    release(l)
+if not lock_dir_exists:  # remove lock directory if it was created here
+    os.rmdir(lock_dir)
