08d5600b738c10dd7698c0002d514b246a0d8cc8
Jeremy R. Manning <jeremy.r.manning@dartmouth.edu>
2017-01-17 18:04:04 -0500

    updated config (pieman)

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ config = dict()
 # job creation options
 
 if (socket.gethostname() == 'vertex') or (socket.gethostname() == 'vertex.kiewit.dartmouth.edu') or (socket.gethostname() == 'vertex.local'):
-    config['datadir'] = '/Users/jmanning/Desktop/fMRI/sherlock/'
+    config['datadir'] = '/Users/jmanning/Desktop/fMRI/pieman/'
     config['workingdir'] = config['datadir']
     config['startdir'] = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))  # directory to start the job in
     config['template'] = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'run_job_local.sh')
 else:
-    config['datadir'] = '/idata/cdl/data/fMRI/sherlock/'
-    config['workingdir'] = '/idata/cdl/jmanning/sherlock_analysis/'
+    config['datadir'] = '/idata/cdl/data/fMRI/pieman/'
+    config['workingdir'] = '/idata/cdl/jmanning/pieman_analysis/'
     config['startdir'] = '/idata/cdl/jmanning'
     config['template'] = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'run_job_cluster.sh')
 
@@ -23,7 +23,7 @@ config['resultsdir'] = os.path.join(config['workingdir'], 'results')
 
 
 # runtime options
-config['jobname'] = "sherlock_decoding"  # default job name
+config['jobname'] = "pieman_decoding"  # default job name
 config['q'] = "largeq"  # options: default, test, largeq
 config['nnodes'] = 1  # how many nodes to use for this one job
 config['ppn'] = 16  # how many processors to use for this one job (assume 4GB of RAM per processor)
