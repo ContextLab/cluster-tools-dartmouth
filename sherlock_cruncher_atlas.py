21e3aa559edc2127aa0682f26a96a2d905d1e9da
Jeremy R. Manning <jeremy.r.manning@dartmouth.edu>
2017-06-20 17:37:57 -0400

    added sherlock atlas analysis

---
+++ sherlock_cruncher_atlas.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import os
+import sys
+import numpy as np
+import scipy.io as sio
+from config2 import config
+from isfc import timepoint_decoder
+import glob
+
+def load_helper(x):
+    return np.array(np.load(x))
+
+iternum = int(sys.argv[1])
+condition = sys.argv[2]
+save_file = os.path.join(config['resultsdir'], 'atlas_results_' + condition + '_' + str(iternum) + '.npz')
+
+if not os.path.isfile(save_file):
+    # xval_groups = np.load(os.path.join(config['resultsdir'], 'xval_folds.npz'))['xval_groups']
+    # params = np.load(os.path.join(config['resultsdir'], 'best_parameters.npz'))
+    fnames = glob.glob(os.path.join(os.path.join(config['datadir'], 'atlas_files'), condition + '*.npy'))
+
+    data = np.array(map(load_helper, fnames))
+    results = timepoint_decoder(data[:, :, np.sum(np.isnan(np.sum(data, axis=0)), axis=0) == 0], windowsize=60, mu=1, nfolds=2)
+    np.savez(save_file, results=results)
