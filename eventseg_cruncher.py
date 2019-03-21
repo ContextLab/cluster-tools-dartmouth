783efb247d4e061d88654b3e8aac4a3071538973
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2019-03-21 18:06:57 -0400

    wrote cruncher script

--- eventseg_cruncher.py
+++ eventseg_cruncher.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python
 
+import sys
 import os
+import pickle
+import numpy as np
+import brainiak.eventseg.event as event
 from config import config
 
-try:
-    os.stat(config['resultsdir'])
-except:
-    os.makedirs(config['resultsdir'])
+filepath, k = sys.argv[1], sys.argv[2]
+dir, f_name  = os.path.split(filepath)
+rectype = os.path.split(dir)[1]
+trajectory = np.load(filepath)
+savepath = os.path.join(config['resultsdir'], rectype, os.path.splitext(f_name)[0], 'k'+k+'.npy')
 
-#TODO: write analysis here
+if not os.path.isfile(savepath):
+    ev = event.EventSegment(k)
+    ev.fit(trajectory)
+
+    np.save(savepath, ev.segments_[0])
