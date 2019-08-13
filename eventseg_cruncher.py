a5a2e745e6e6c6619e32cfd94be6335499afa759
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2019-08-13 14:53:06 -0400

    updated main code

--- eventseg_cruncher.py
+++ eventseg_cruncher.py
@@ -1,20 +1,40 @@
 #!/usr/bin/python
 
 import sys
-import os
-import pickle
+import joblib
 import numpy as np
-import brainiak.eventseg.event as event
+import pandas as pd
+from scipy.signal import resample
 from eventseg_config import config
+from helpers import *
 
-filepath, k = sys.argv[1], sys.argv[2]
-dir, f_name  = os.path.split(filepath)
-rectype = os.path.split(dir)[1]
-trajectory = np.load(filepath)
-savepath = os.path.join(config['resultsdir'], rectype, os.path.splitext(f_name)[0], 'k'+k+'.npy')
+id = sys.argv[1]
+wsize = 50
 
-if not os.path.isfile(savepath):
-    ev = event.EventSegment(int(k))
-    ev.fit(trajectory)
+# load only single row to save time & memory
+skiprows = range(1, id)
+data = pd.read_csv(os.path.join(config['datadir'], 'data.csv'), skiprows=skiprows, nrows=1).T.squeeze()
+name = data.title
 
-    np.save(savepath, ev.segments_[0])
+# remove HTML formatting, clean script content
+clean_script = cleanup_text(wipe_formatting(data.script))
+
+# don't model empty scripts (8,528 characters is length of shortest cleaned script)
+if len(clean_script) < 8528:
+    sys.exit()
+
+cv = joblib.load(os.path.join(config['datadir'], 'fit_cv.joblib'))
+lda = joblib.load(os.path.join(config['datadir'], 'fit_lda_t100.joblib'))
+
+sentences = cleaned.split('.')
+windows = []
+for ix, _ in enumerate(sentences):
+    windows.append(' '.join(sentences[ix:ix+wsize]))
+
+
+script_tf = cv.transform(windows)
+script_traj = resample(lda.transform(script_tf), 1000)
+corrmat = np.corrcoef(script_traj)
+
+np.save(os.path.join(config['datadir'], 'trajectories', f'{name}_traj.npy'), script_traj)
+np.save(os.path.join(config['datadir'], 'corrmats', f'{name}_corrmat.npy'), corrmat)
