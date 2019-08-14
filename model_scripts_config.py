4ab131339bc3c4046ee8d825bef14d660230e3f6
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2019-08-14 11:55:24 -0400

    updated config

--- model_scripts_config.py
+++ model_scripts_config.py
@@ -8,15 +8,16 @@ config = dict()
 
 #add additional checks for your local machine here...
 # ******** check kiewit hostname from eduroam ********
-if (socket.gethostname() == 'Paxtons-MacBook-Pro') or (socket.gethostname() == 'Paxtons-MacBook-Pro.kiewit.dartmouth.edu') or (socket.gethostname() == 'Paxtons-MacBook-Pro.local'):
+hostname = socket.gethostname().lower()
+if ('local' in hostname) or ('paxton' in hostname) or ('vpn' in hostname):
     config['datadir'] = '/Users/paxtonfitzpatrick/Documents/Dartmouth/CDL/MIND-2019/narrative_complexity/data'
     config['workingdir'] = config['datadir']
     config['startdir'] = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))  # directory to start the job in
     config['template'] = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'run_job_local.sh')
 else:
-    config['datadir'] = '/dartfs/rc/lab/D/DBIC/CDL/data/movie_scripts/'
-    config['startdir'] = '/dartfs/rc/lab/D/DBIC/CDL/f0028ph/MIND-narrative-modeling'
-    config['workingdir'] = os.path.join(startdir, 'cluster-scripts')
+    config['datadir'] = '/dartfs/rc/lab/D/DBIC/CDL/f0028ph/MIND-narrative-modeling/data/'
+    config['startdir'] = '/dartfs/rc/lab/D/DBIC/CDL/f0028ph/MIND-narrative-modeling/'
+    config['workingdir'] = os.path.join(config['startdir'], 'cluster-scripts')
     config['template'] = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'run_job_cluster.sh')
 
 config['scriptdir'] = os.path.join(config['workingdir'], 'scripts')
