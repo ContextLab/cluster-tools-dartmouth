08fa40b1d51872d466e65f06fb3920e901933c30
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-01-14 17:58:06 -0500

    wrote function to upload scripts to cluster

---
+++ upload_scripts.py
@@ -0,0 +1,54 @@
+import os
+from os.path import join as opj
+from spurplus import connect_with_retries
+from _helpers import (
+    parse_config,
+    prompt_input,
+    md5_checksum
+)
+from cluster_scripts.config import job_config
+
+
+def upload_scripts(remote_shell,
+                   local_script_dir,
+                   job_config,
+                   confirm_overwrite=True):
+
+    remote_startdir = job_config['startdir']
+    remote_workingdir = job_config['workingdir']
+    remote_datadir = job_config['datadir']
+
+    to_upload = os.listdir(local_script_dir)
+    # ignore hidden files (e.g., .DS_Store on MacOS)
+    to_upload = [f for f in to_upload if not f.startswith('.')]
+    for remote_dir in [remote_startdir, remote_workingdir, remote_datadir]:
+        try:
+            remote_shell.is_dir(remote_dir)
+        except FileNotFoundError:
+            # is_dir method raises exception if path doesn't exist
+            print(f'creating remote directory: {remote_dir}')
+            remote_shell.mkdir(remote_dir)
+
+    print("uploading files...")
+    for file in to_upload:
+        src_path = opj(local_script_dir, file)
+        dest_path = opj(remote_workingdir, file)
+        if remote_shell.exists(dest_path):
+            # don't bother uploading file if it hasn't been edited
+            local_checksum = md5_checksum(src_path)
+            remote_checksum = remote_shell.md5(dest_path)
+            if local_checksum == remote_checksum:
+                print(f"skipping {file} (no changes)")
+                continue
+
+            if confirm_overwrite:
+                # prompt for confirmation of overwrite if option is enabled
+                question = f"{file}: overwrite remote version with local changes?"
+                overwrite_confirmed = prompt_input(question)
+                if not overwrite_confirmed:
+                    print(f"Skipping {file} (overwrite declined)")
+                    continue
+
+        remote_shell.put(src_path, dest_path, create_directories=False)
+
+
