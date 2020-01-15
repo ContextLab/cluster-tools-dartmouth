7f7f7dfa4a6440ce1d1bf55817cd380ed372c533
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-01-14 22:08:52 -0500

    added fallback function to try to load config file if not provided

--- _helpers.py
+++ _helpers.py
@@ -1,6 +1,56 @@
 import hashlib
+import os
+from os.path import realpath, join as opj, sep as pathsep
 import sys
 from configparser import ConfigParser
+
+
+def md5_checksum(filepath):
+    """
+    computes the MD5 checksum of a local file to compare against remote
+
+    NOTE: MD5 IS CONSIDERED CRYPTOGRAPHICALLY INSECURE
+    (see https://en.wikipedia.org/wiki/MD5#Security)
+    However, it's still very much suitable in cases (like ours) where one
+    wouldn't expect **intentional** data corruption
+    """
+    hash_md5 = hashlib.md5()
+    with open(filepath, 'rb') as f:
+        # avoid having to read the whole file into memory at once
+        for chunk in iter(lambda: f.read(4096), b''):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+
+def attempt_load_config():
+    splitpath = realpath(__file__).split(pathsep)
+    try:
+        try:
+            # get path to project root directory
+            splitroot = splitpath[: splitpath.index('cluster-tools-dartmouth') + 1]
+            project_root = pathsep.join(splitroot)
+            config_dir = opj(project_root, 'configs')
+        except ValueError as e:
+            # pass exceptions onto broad outer exception for function
+            raise FileNotFoundError(f"cluster-tools-dartmouth not found in path\
+             {realpath(__file__)}").with_traceback(e.__traceback__)
+
+        configs = os.listdir(config_dir)
+        # filter out hidden files and the template config
+        configs = [f for f in configs if not (f.startswith('template') or f.startswith('.'))]
+        if len(configs) == 1:
+            config_path = opj(config_dir, configs[0])
+            config = parse_config(config_path)
+            return config
+        else:
+            # fail if multiple or no config files are found
+            raise FileNotFoundError(f"Unable to determine which config file to \
+            read from {len(configs)} choices in {config_dir}")
+
+    except FileNotFoundError as e:
+        raise FileNotFoundError("Failed to load config file from expected \
+        location").with_traceback(e.__traceback__)
+
 
 def parse_config(config_path):
     """
@@ -49,20 +99,3 @@ def prompt_input(question, default=None):
         else:
             sys.stdout.write("Please respond with either 'yes' (or 'y') \
             or 'no' (or 'n')\n")
-
-
-def md5_checksum(filepath):
-    """
-    computes the MD5 checksum of a local file to compare against remote
-
-    NOTE: MD5 IS CONSIDERED CRYPTOGRAPHICALLY INSECURE
-    (see https://en.wikipedia.org/wiki/MD5#Security)
-    However, it's still very much suitable in cases (like ours) where one
-    wouldn't expect **intentional** data corruption
-    """
-    hash_md5 = hashlib.md5()
-    with open(filepath, 'rb') as f:
-        # avoid having to read the whole file into memory at once
-        for chunk in iter(lambda: f.read(4096), b''):
-            hash_md5.update(chunk)
-    return hash_md5.hexdigest()
