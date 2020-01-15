bfbd2a2d87830adce70f5d80d4da870dc60dafa8
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-01-14 23:23:00 -0500

    moved all config opts about environment to job_config

--- cluster_scripts/config.py
+++ cluster_scripts/config.py
@@ -20,5 +20,6 @@ job_config['ppn'] = # (int) how many processors to use for this one job (assume 
 job_config['walltime'] =  # (str) maximum runtime, in h:MM:SS (e.g., "10:00:00")
 job_config['cmd_wrapper'] =  # (str) replace with actual command wrapper (e.g. "python", "matlab", etc.)
 job_config['modules'] = # (str) modules you need to load for your scripts separated by a space (e.g., "python matlab")
+job_config['env_type'] = # (str) what kind of Python environment you use (NOTE: sole option is currently conda -- venv and virtualenv coming soon!)
 job_config['env_name'] = # (str) names of (currently, conda) environment you want your submission script and jobs to run in
 # ====== MODIFY ONLY THE CODE BETWEEN THESE LINES ======
