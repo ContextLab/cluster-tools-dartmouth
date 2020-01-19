de17f82228546d69cd84615f1fba814ce719687a
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-01-19 17:36:25 -0500

    add options for configuring email notifications

--- cluster_scripts/config.py
+++ cluster_scripts/config.py
@@ -3,8 +3,8 @@ from os.path import dirname, realpath, join as opj
 job_config = dict()
 
 # ====== MODIFY ONLY THE CODE BETWEEN THESE LINES ======
-# job creation options
-job_config['startdir'] = # path to the remote foler for this project.  Should be something like /dartfs/rc/lab/D/DBIC/CDL/<your_net_id>/<project_name>
+# directory setup (NOTE: it's best to modify only 'startdir'
+job_config['startdir'] = # (str) path to the remote foler for this project. Should be something like /dartfs/rc/lab/D/DBIC/CDL/<your_net_id>/<project_name>
 job_config['datadir'] = opj(job_config['startir'], 'data')
 job_config['workingdir'] = opj(job_config['startir'], 'scripts')
 job_config['template'] = opj(dirname(realpath(__file__)), 'run_job_cluster.sh')
@@ -13,7 +13,7 @@ job_config['lockdir'] = opj(job_config['workingdir'], 'locks')
 
 # runtime options
 job_config['jobname'] = # (str) default job name
-job_config['q'] =   # (str) options: default, test, largeq (when in doubt, use "largeq")
+job_config['queue'] =   # (str) options: default, test, largeq (when in doubt, use "largeq")
 job_config['nnodes'] = # (int) how many nodes to use for this one job
 job_config['ppn'] = # (int) how many processors to use for this one job (assume 4GB of RAM per processor)
 job_config['walltime'] =  # (str) maximum runtime, in h:MM:SS (e.g., "10:00:00")
@@ -21,4 +21,7 @@ job_config['cmd_wrapper'] =  # (str) replace with actual command wrapper (e.g. "
 job_config['modules'] = # (str) modules you need to load for your scripts separated by a space (e.g., "python matlab")
 job_config['env_type'] = # (str) what kind of Python environment you use (NOTE: sole option is currently "conda" -- "venv" and "virtualenv" coming soon!)
 job_config['env_name'] = # (str) names of (currently, conda) environment you want your submission script and jobs to run in
+job_config['email_updates'] = # (str) what events you want to receive email notifications about (see options below)
+job_config['email_addr'] = # (str) email address where you want job notifications sent
+
 # ====== MODIFY ONLY THE CODE BETWEEN THESE LINES ======
