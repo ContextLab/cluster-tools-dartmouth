b9b8a415eacfa52ed155929c0bbe1ff22ea46e9c
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-01-20 18:14:02 -0500

    helper for writing submit job script

--- _helpers.py
+++ _helpers.py
@@ -2,6 +2,7 @@ import hashlib
 import os
 import sys
 from os.path import isfile, realpath, join as opj, sep as pathsep
+from string import Template
 from configparser import ConfigParser
 
 
@@ -24,7 +25,9 @@ def attempt_load_config():
 
         configs = os.listdir(config_dir)
         # filter out hidden files and the template config
-        configs = [f for f in configs if not (f.startswith('template') or f.startswith('.'))]
+        configs = [f for f in configs if not (f.startswith('template')
+                                              or f.startswith('.'))
+                   ]
         if len(configs) == 1:
             config_path = opj(config_dir, configs[0])
             config = parse_config(config_path)
@@ -120,3 +123,61 @@ def prompt_input(question, default=None):
         else:
             sys.stdout.write("Please respond with either 'yes' (or 'y') \
             or 'no' (or 'n')\n")
+
+
+def write_remote_submitter(remote_shell, job_config, env_activate_cmd, env_deactivate_cmd, submitter_walltime):
+    remote_dir = job_config['workingdir']
+    submitter_fpath = opj(remote_dir, 'submit_jobs.sh')
+
+
+    try:
+        assert remote_shell.is_dir(remote_dir)
+    except AssertionError as e:
+        raise ValueError(
+            f"Can't create job submission script in dir: {remote_dir}. \
+            Intended directory is an existing file."
+        ).with_traceback(e.__traceback__)
+    except FileNotFoundError as e:
+        raise FileNotFoundError(
+            f"Can't create job submission script in dir: {remote_dir}. \
+            Intended directory does not exist."
+        ).with_traceback(e.__traceback__)
+
+
+    template_vals = {
+        'jobname': job_config['jobname'],
+        'walltime': submitter_walltime,
+        'modules': job_config['modules'],
+        'activate_cmd': env_activate_cmd,
+        'deactivate_cmd': env_deactivate_cmd,
+        'env_name': job_config['env_name'],
+        'cmd_wrapper': job_config['cmd_wrapper'],
+        'submitter_script': opj(remote_dir, 'submit.py')
+    }
+
+    template = Template(
+        """
+#!/bin/bash -l
+        
+#PBS -N ${jobname}-submitter
+#PBS -q default
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=${walltime}
+#PBS -m bea
+        
+module load $modules
+$activate_cmd $env_name
+        
+$cmd_wrapper $submitter_script
+        
+$deactivate_cmd
+        """
+    )
+
+    content = template.substitute(template_vals)
+    remote_shell.write_text(submitter_fpath, content)
+    return submitter_fpath
+
+
+
+
