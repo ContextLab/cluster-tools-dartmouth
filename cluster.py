da2a03df2ec32caa8f3ef65838ad1a3f9f5d3b7c
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-10-31 07:31:15 -0400

    added typing

--- cluster.py
+++ cluster.py
@@ -1,26 +1,54 @@
 import getpass
 import os
 from io import StringIO
+from pathlib import Path
+from typing import (
+    Optional,
+    Union,
+    TextIO,
+    Dict,
+    Sequence,
+)
 
 import spur
 import spurplus
 
 
+PathLike = Union[str, Path]
+
+
 class Cluster:
     def __init__(
             self,
-            hostname,
-            username=None,
-            password=None,
-            use_key=False,
-            port=22,
-            timeout=60,
-            retries=0,
-            retry_delay=1,
-            shell=None,
-            cwd=None,
-            env_additions=None
-    ):
+            hostname: str,
+            username: Optional[str] = None,
+            password: Optional[str] = None,
+            use_key: bool = False,
+            port: int = 22,
+            timeout: int = 60,
+            retries: int = 0,
+            retry_delay: int = 1,
+            shell: Optional[PathLike] = None,
+            cwd: Optional[PathLike] = None,
+            env_additions: Optional[Dict[str, str]] = None
+    ) -> None:
+        """
+        TODO: add docstring
+
+        Parameters
+        ----------
+        hostname
+        username
+        password
+        use_key
+        port
+        timeout
+        retries
+        retry_delay
+        shell
+        cwd
+        env_additions
+        """
         # setup connection first for fast failure
         if hostname == 'localhost':
             self.shell = spur.LocalShell()
@@ -28,16 +56,14 @@ class Cluster:
             self.port = None
             self.environ = dict(os.environ)
         else:
-            self.shell = spurplus.connect_with_retries(
-                hostname=hostname,
-                username=username,
-                password=(getpass.getpass('Password: ') if not (use_key or password) else password),
-                look_for_private_keys=(not use_key),
-                port=port,
-                connect_timeout=timeout,
-                retries=retries,
-                retry_period=retry_delay
-            )
+            self.shell = spurplus.connect_with_retries(hostname=hostname,
+                                                       username=username,
+                                                       password=(getpass.getpass('Password: ') if not (use_key or password) else password),
+                                                       look_for_private_keys=(not use_key),
+                                                       port=port,
+                                                       connect_timeout=timeout,
+                                                       retries=retries,
+                                                       retry_period=retry_delay)
             self.username = username
             self.port = port
             env_str = self.shell.run(['printenv']).output.split('\nBASH_FUNC_module()')[0]
@@ -66,11 +92,12 @@ class Cluster:
         return self.shell.__exit__(*args)
 
     @property
-    def cwd(self):
+    def cwd(self) -> str:
         return self._cwd
 
     @cwd.setter
-    def cwd(self, new_cwd):
+    def cwd(self, new_cwd: PathLike) -> None:
+        new_cwd = str(new_cwd)
         if not new_cwd.startswith('/'):
             raise AttributeError('working directory must be an absolute path')
         try:
@@ -86,11 +113,12 @@ class Cluster:
             self._cwd = new_cwd
 
     @property
-    def SHELL(self):
+    def SHELL(self) -> str:
         return self._SHELL
 
     @SHELL.setter
-    def SHELL(self, new_shell):
+    def SHELL(self, new_shell: PathLike) -> None:
+        new_shell = str(new_shell)
         shells_avail = self.shell.run(['cat', '/etc/shells']).output.splitlines()
         if new_shell in shells_avail:
             self._SHELL = new_shell
@@ -102,18 +130,26 @@ class Cluster:
                 raise AttributeError(f"No executable found for {new_shell}. "
                                      f"Available shells are:\n{', '.join(shells_avail)}")
 
-    def getenv(self, var, default=None):
+    def getenv(self, var: str, default: Optional[str] = None) -> str:
         return self.environ.get(var, default=default)
 
-    def putenv(self, var, value):
+    def putenv(self, var: str, value: str) -> None:
         self.environ[var] = value
 
-    def unsetenv(self, var):
+    def unsetenv(self, var: str) -> None:
         self.environ.pop(var)
 
-    def spawn_process(self, command, stdout=None, stderr=None, stream_encoding='utf-8', tty=False):
+    def spawn_process(
+            self,
+            command: Union[str, list],
+            stdout: Optional[TextIO] = None,
+            stderr=None,
+            stream_encoding='utf-8',
+            tty=False
+    ) -> 'RemoteProcess':
         # might just be shortcut for self.exec_command(block=True)
         # if stdout/stderr are None, default to just writing to the object
+
 
 
 
@@ -121,15 +157,15 @@ class Cluster:
 class RemoteProcess:
     def __init__(
             self,
-            command,
-            ssh_shell,
-            working_dir=None,
-            stdout=None,
-            stderr=None,
-            stream_encoding='utf-8',
-            env_updates=None,
-            tty=False
-    ):
+            command: Sequence[str],
+            ssh_shell: Union[spurplus.SshShell, spur.SshShell],
+            working_dir: PathLike = None,
+            stdout: Optional[TextIO] = None,
+            stderr: Optional[TextIO] = None,
+            stream_encoding: str = 'utf-8',
+            env_updates: Optional[Dict[str, str]] = None,
+            tty: bool = False
+    ) -> None:
         self.command = command
         self.ssh_shell = ssh_shell
         self.working_dir = working_dir
@@ -139,11 +175,19 @@ class RemoteProcess:
         self.env_updates = env_updates
         self.has_tty = tty
 
+        self._proc = None
         self.pid = None
+
         self.started = False
         self.completed = False
 
-
     def run(self):
-        self.ssh_shell.spawn(command=self.command, cwd=self.working_dir, env_updates=None, stdout=self.stdout, stderr=self.stderr)
+        self._proc = self.ssh_shell.spawn(command=self.command,
+                                          cwd=self.working_dir,
+                                          env_updates=None,
+                                          stdout=self.stdout,
+                                          stderr=self.stderr,
+                                          store_pid=True)
+        self.pid =
+
 
