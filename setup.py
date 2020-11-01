0ceef19ff5b9b6e3203f56c2372cad78dd9f4c50
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-11-01 00:17:05 -0400

    setup scripts

---
+++ setup.py
@@ -0,0 +1,7 @@
+# TODO: We should eventually switch to PEP 517-compliant install via
+#  pyproject.toml, but I'm sticking with this for development since
+#  setup.py is still required for editable installs during
+from setuptools import setup
+
+
+setup()
