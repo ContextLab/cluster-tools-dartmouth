2be3a65340821f29259c0f10a78782049d43b893
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-11-01 01:30:52 -0500

    finished setup scripts

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
-# TODO: We should eventually switch to PEP 517-compliant install via
+# TODO:
+#  We should eventually switch to PEP 517-compliant install via
 #  pyproject.toml, but I'm sticking with this for development since
-#  setup.py is still required for editable installs during
+#  setup.py is still required for editable installs during.
+#  NOTE:
+#  this will hopefully be resolved soon. Putting this here for later
+#  reference:
+#  https://github.com/pypa/pip/pull/6370#issuecomment-479938348
 from setuptools import setup
 
 
