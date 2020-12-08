9a93e81544ce328228980ac243e50cf99afd9d12
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-12-08 18:10:13 -0500

    class for representing project scripts (cruncher, collector, etc.)

--- clustertools/project/project_script.py
+++ clustertools/project/project_script.py
@@ -1,10 +1,29 @@
-# """
-# class that represents submitter, cruncher, collector, etc. scripts
-# """
-#
-#
-# class Script:
-#     def __init__(
-#             self,
-#             script_type:
-#     ):
+"""
+class that represents submitter, cruncher, collector, etc. scripts
+"""
+from pathlib import Path, PurePosixPath
+from typing import Optional
+
+from clustertools.mixins.synced_file import SyncedFile
+from clustertools.project.project import Project
+from clustertools.shared.typing import PathLike
+
+
+class ProjectScript(SyncedFile):
+    def __init__(
+            self,
+            project: Project,
+            local_path: PathLike,
+            remote_path: Optional[PathLike] = None
+    ):
+        self.project = project
+        self.local_path = Path(local_path).resolve()
+        if remote_path is None:
+            filename = self.local_path.name
+            self.remote_path = self.project.root_dir.joinpath(filename)
+        else:
+            self.remote_path = self.project.cluster.resolve_path(PurePosixPath(remote_path))
+        self.content = self.local_path.read_text()
+
+    def __repr__(self):
+        return f'{self.local_path} -> {self.remote_path}\n\n{self.content}'
