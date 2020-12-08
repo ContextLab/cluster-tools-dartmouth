f718d38b8e1b4462cb8aff19bafc9af91dd5e22c
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-12-08 18:12:50 -0500

    base class for files that are synced between local and remote

--- clustertools/__init__.py
+++ clustertools/__init__.py
@@ -1,9 +1,12 @@
-from clustertools.ssh_shell import SshShell
+from pathlib import Path
 from clustertools.cluster import Cluster
-from clustertools.batch_submission import BatchSubmission
-
 
 version_info = (0, 0, 1)
 __version__ = '.'.join(map(str, version_info))
 
+CLUSTERTOOLS_CONFIG_DIR = Path.home().joinpath('.clustertools')
+
 # TODO: add logging functionality
+# TODO: move all property setter docstrings to getters
+# TODO: update type checking-related imports to conditionally import if
+#  typing.TYPE_CHECKING is True to avoid circular imports
