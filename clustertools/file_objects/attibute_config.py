6c4e96cce4ab8a4cdd2d4a6b6167386f1599cc0f
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-12-09 12:21:32 -0500

    updated AttributeConfig to take a callback function for updates to config dict, reworked __setattr__ around single callback run for self.update

--- clustertools/file_objects/attibute_config.py
+++ clustertools/file_objects/attibute_config.py
@@ -1,4 +1,4 @@
-from typing import Any, MutableMapping, NoReturn, Optional
+from typing import Any, Callable, MutableMapping, NoReturn, Optional
 
 
 class AttributeConfig(dict):
@@ -30,14 +30,26 @@ class AttributeConfig(dict):
     #     These proxied attribute names will *NOT* override attr/method
     #     names defined on the owner class *OR* any parent classes or
     #     mixins
-    def __init__(self, d: MutableMapping[Any, Any], **kwargs) -> None:
-        d.update(kwargs)
+
+    @staticmethod
+    def _default_config_update_hook():
+        pass
+
+    def __init__(
+            self,
+            d: MutableMapping[Any, Any],
+            update_hook: Optional[Callable[[], None]] = None
+    ) -> None:
         super().__init__(d)
         for key, value in d.items():
-            if hasattr(value, 'keys'):
-                value = AttributeConfig(value)
-            # using dict method directly gets around "frozen" self.__setitem__
+            if hasattr(value, 'keys') and key != 'environ':
+                value = AttributeConfig(value, update_hook=update_hook)
+            # using dict methods directly to get around "frozen" self.__setitem__
             dict.__setitem__(self, key, value)
+        if update_hook is None:
+            update_hook = AttributeConfig._default_config_update_hook
+        print(update_hook)
+        dict.__setattr__(self, '_config_update_hook', update_hook)
 
     def __delattr__(self, name: str) -> NoReturn:
         raise TypeError("'AttributeConfig' object does not support key deletion")
@@ -65,14 +77,16 @@ class AttributeConfig(dict):
     def __getitem__(self, name: str) -> Any:
         return self.__getattr__(name)
 
-    def __setattr__(self, name, value):
+    def __setattr_helper_(self, name, value):
+        # contains all of the "real" self.__setattr__ machinery
         try:
             curr_value = dict.__getitem__(self, name)
         except KeyError as e:
             for _value in self.values():
                 if isinstance(_value, AttributeConfig):
                     try:
-                        return _value.__setattr__(name, value)
+                        print(f'upper return with name = {name}, value = {value}')
+                        return _value.__setattr_helper_(name, value)
                     except KeyError:
                         continue
                     except (AttributeError, TypeError):
@@ -87,31 +101,45 @@ class AttributeConfig(dict):
             elif not isinstance(value, type(curr_value)):
                 raise TypeError(f"Value assigned to '{name}' must be of type "
                                 f"'{type(curr_value)}'")
+            print(f'lower return with name = {name}, value = {value}')
             return dict.__setitem__(self, name, value)
+
+    def __setattr__(self, name, value):
+        # main self.__setattr__ exists as a wrapper to prevent multiple
+        # calls to self._config_update_hook within self.update
+        try:
+            print('before __setattr_helper_')
+            self.__setattr_helper_(name, value)
+        except:
+            raise
+        else:
+            print('outer return')
+            self._config_update_hook()
 
     def __setitem__(self, name: str, value: Any) -> None:
         return self.__setattr__(name, value)
 
     # noinspection PyPep8Naming
-    def update(self, *E, **F) -> None:
+    def update(self, E, **F) -> None:
         # params named to match dict.update() docstring/signature
         # expensive but safe approach, prevents all changes if one assignment from F fails
-        E = dict(E[0])
-        E.update(F)
+        E = dict(E, **F)
         pre_update = dict()
         try:
             for key, value in E.items():
                 # record each pre-self.update value
                 pre_update[key] = self.__getattr__(key)
                 # manually update the fields one-by-one
-                self.__setattr__(key, value)
+                self.__setattr_helper_(key, value)
         except Exception:
             # if any assignment fails, restore already-updated fields
             # remove the problem key (if recorded) so we don't raise another AttributeError
             pre_update.pop(key, None)
             for key, old_value in pre_update.items():
-                self.__setattr__(key, old_value)
+                dict.__setattr__(self, key, old_value)
             raise
+        else:
+            self._config_update_hook()
 
     def setdefault(self, key: Any, default: Optional[Any] = None) -> NoReturn:
         raise TypeError("'AttributeConfig' object does not support key insertion")
