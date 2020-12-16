09615a60ae11a2e5485467f55183adc3332bc308
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-12-16 11:03:34 -0500

    renamed virtual environment activate/deactivate config fields

--- clustertools/file_objects/script.py
+++ clustertools/file_objects/script.py
@@ -2,17 +2,20 @@ from typing import Optional
 
 from clustertools.cluster import Cluster
 from clustertools.file_objects.synced_file import SyncedFile
+from clustertools.project.project import Project
 from clustertools.shared.typing import PathLike
 
 
 class ProjectScript(SyncedFile):
     def __init__(
             self,
-            cluster: Cluster,
+            project: Project,
             local_path: PathLike,
             remote_path: Optional[PathLike] = None
-    ):
+    ) -> None:
+        cluster = project._cluster
         super().__init__(cluster=cluster, local_path=local_path, remote_path=remote_path)
+        self.project = project
 
     @property
     def content(self) -> str:
@@ -20,6 +23,15 @@ class ProjectScript(SyncedFile):
         # class will be used with, so it's worth this being a property
         # so it's always up-to-date
         return self.local_path.read_text()
+
+    @property
+    def expects_args(self) -> bool:
+        content = self.content
+        return (
+                "sys.argv" in content
+                or "from sys import argv" in content
+                or "argparse" in content
+        )
 
     def __repr__(self):
         return f'{self.local_path} -> {self.remote_path}\n\n{self.content}'
